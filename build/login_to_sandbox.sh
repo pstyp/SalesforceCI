5c0f885127e8e37b474dc57e4029950132d531ff
Pstypulkowski <pstyp94@gmail.com>
2021-07-27 09:50:12 +0100

    made changes

--- build/login_to_sandbox.sh
+++ build/login_to_sandbox.sh
@@ -2,5 +2,5 @@ echo "Logging into Sandbox Salesforce Org"
 mkdir keys
 echo $SANDBOX_CERT_KEY | base64 -di > keys/server.key
 
-echo "Authenticating org"
+echo "Authenticating orgs"
 sfdx force:auth:jwt:grant --clientid $SANDBOX_APP_KEY --jwtkeyfile keys/server.key --username $SANDBOX_USERNAME --setdefaultdevhubusername -a DevHub
